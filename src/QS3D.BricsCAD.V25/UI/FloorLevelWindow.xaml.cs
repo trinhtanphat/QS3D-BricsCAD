3a488ab25d320d62852afbf997eda6f089ca2469
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-10 23:07:48 +0700

    fix(ui): bind level picker lifetime to drawing

--- src/QS3D.BricsCAD.V25/UI/FloorLevelWindow.xaml.cs
+++ src/QS3D.BricsCAD.V25/UI/FloorLevelWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace QS3D.BricsCAD.V25.UI
         {
             _document = document ?? throw new ArgumentNullException(nameof(document));
             InitializeComponent();
+            DocumentBoundWindowLifetime.Attach(this, _document);
             Loaded += (_, __) => RefreshAll();
         }
 
