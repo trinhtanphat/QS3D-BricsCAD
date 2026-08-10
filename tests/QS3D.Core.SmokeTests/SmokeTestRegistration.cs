9c172707e586d7e288800bec2abe79cdabb5a727
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-10 17:48:58 +0700

    test(geometry): register polygon scanline smoke

--- tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
@@ -39,6 +39,7 @@ namespace QS3D.Core.SmokeTests
             CurtainFrameOpeningHealthSmoke.Run();
             BomReleaseGuardSmoke.Run();
             LevelReferenceSmoke.Run();
+            PolygonScanlineClipperSmoke.Run();
         }
     }
 }
