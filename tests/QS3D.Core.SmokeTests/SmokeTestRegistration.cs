7b39746f1a9101d4018413d8deb38fb56ba58a6d
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-10 07:10:55 +0700

    test(review): register hardening smoke suite

--- tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
@@ -10,6 +10,7 @@ namespace QS3D.Core.SmokeTests
             ProjectQuantitySmoke.Run();
             PersistenceHardeningSmoke.Run();
             AdvancedDomainSmoke.Run();
+            ReviewHardeningSmoke.Run();
         }
     }
 }
