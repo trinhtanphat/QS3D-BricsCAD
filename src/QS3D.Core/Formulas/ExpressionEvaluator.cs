ba7ec7b06c52f014f065c535bceeb43a2205e4a1
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-12 08:49:43 +0700

    fix(formulas): return readonly referenced variables

--- src/QS3D.Core/Formulas/ExpressionEvaluator.cs
+++ src/QS3D.Core/Formulas/ExpressionEvaluator.cs
@@ -51,7 +51,7 @@ namespace QS3D.Core.Formulas
                 }
                 index++;
             }
-            return result;
+            return result.AsReadOnly();
         }
 
         private static void ValidateExpression(string expression)
