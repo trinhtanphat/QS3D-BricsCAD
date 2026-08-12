678c6a547b3fa31fcb5c9016856200c16ae8e7a1
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-12 15:11:04 +0700

    test(formulas): cover numeric literal underflow

--- tests/QS3D.Core.SmokeTests/FormulaFiniteSafetySmoke.cs
+++ tests/QS3D.Core.SmokeTests/FormulaFiniteSafetySmoke.cs
@@ -33,8 +33,15 @@ namespace QS3D.Core.SmokeTests
                 () => evaluator.Evaluate("1e-300 / 1e300"));
             Contains("Division underflowed to zero.", divisionUnderflow.Message);
 
+            var literalUnderflow = Capture<InvalidOperationException>(
+                () => evaluator.Evaluate("1e-4000"));
+            Contains("Number '1e-4000' underflowed to zero.", literalUnderflow.Message);
+
             Near(5d, evaluator.Evaluate("min(10, 5)"), 1e-12);
             Near(6d, evaluator.Evaluate("2 * 3"), 1e-12);
+            Near(0d, evaluator.Evaluate("0"), 0d);
+            Near(0d, evaluator.Evaluate("0e-4000"), 0d);
+            Near(double.Epsilon, evaluator.Evaluate("5e-324"), 0d);
             Near(0d, evaluator.Evaluate("0 * 1e-300"), 0d);
             Near(0d, evaluator.Evaluate("0 / 1e300"), 0d);
             Near(1d, evaluator.Evaluate(new string('-', 64) + "1"), 1e-12);
