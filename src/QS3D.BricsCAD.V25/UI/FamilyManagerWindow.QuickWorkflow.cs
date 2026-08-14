7ccd75c52a26d038e80f28ab82a80ca688662d2a
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-14 15:37:53 +0700

    fix(ui): import canonical CAD project context for Family quick workflow

--- src/QS3D.BricsCAD.V25/UI/FamilyManagerWindow.QuickWorkflow.cs
+++ src/QS3D.BricsCAD.V25/UI/FamilyManagerWindow.QuickWorkflow.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using QS3D.BricsCAD.V25.Cad;
 using QS3D.Core.Audit;
 using QS3D.Core.Domain;
 using QS3D.Core.Persistence;
