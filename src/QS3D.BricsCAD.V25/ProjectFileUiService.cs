663990f5611c6ab1c57c3b3e27f77c31fbeec2f5
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-16 10:54:23 +0700

    fix(build): import WPF message box enums

--- src/QS3D.BricsCAD.V25/ProjectFileUiService.cs
+++ src/QS3D.BricsCAD.V25/ProjectFileUiService.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Windows;
 using Bricscad.ApplicationServices;
 using Microsoft.Win32;
 using QS3D.BricsCAD.V25.UI;
