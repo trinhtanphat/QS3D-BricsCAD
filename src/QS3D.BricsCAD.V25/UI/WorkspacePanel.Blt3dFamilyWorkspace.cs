036c4a8f24d70e3f7ea85c3533dc7abca91c962c
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-16 11:28:01 +0700

    restore workspace placeholder

--- src/QS3D.BricsCAD.V25/UI/WorkspacePanel.Blt3dFamilyWorkspace.cs
+++ src/QS3D.BricsCAD.V25/UI/WorkspacePanel.Blt3dFamilyWorkspace.cs
@@ -1,1 +1,16 @@
-placeholder
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Threading;
+using QS3D.Core.Domain;
+
+namespace QS3D.BricsCAD.V25.UI
+{
+    public partial class WorkspacePanel
+    {
+        private static readonly string[] Blt3dFoundationTreeOrder = { "Cọc", "Đài Cọc", "Dầm Móng", "Móng Băng", "Móng Bè", "Bê Tông Lót" };
+    }
+}
