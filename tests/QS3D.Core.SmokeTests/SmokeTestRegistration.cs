555a3de545731bf18f9e9c0cfe2f8536cf6caabb
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-16 20:07:33 +0700

    test(diagnostics): register BOM bounded input smoke

--- tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
@@ -74,6 +74,7 @@ namespace QS3D.Core.SmokeTests
             CurtainWallOpeningFramePlannerSmoke.Run();
             CurtainFrameOpeningHealthSmoke.Run();
             BomReleaseGuardSmoke.Run();
+            BomReleaseGuardBoundedInputSmoke.Run();
             HealthSummaryReadinessSmoke.Run();
             DependencyHealthMissingTargetSmoke.Run();
             DependencyHealthBlankTargetSmoke.Run();
