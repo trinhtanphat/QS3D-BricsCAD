7d190139df8e3b6b7e964c42cda0c944c16edfb6
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-17 01:16:26 +0700

    test(templates): register required-attribute smoke

---
+++ tests/QS3D.Core.SmokeTests/TemplateProfileRequiredAttributeCanonicalityRegistration.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+
+namespace QS3D.Core.SmokeTests
+{
+    internal static class TemplateProfileRequiredAttributeCanonicalityRegistration
+    {
+        [ModuleInitializer]
+        internal static void Initialize() => TemplateProfileRequiredAttributeCanonicalitySmoke.Run();
+    }
+}
