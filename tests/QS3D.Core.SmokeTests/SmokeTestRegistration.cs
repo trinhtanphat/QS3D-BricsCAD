eef4d412461a4314a6dcb16a30830891fbe7d1cb
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-18 19:45:43 +0700

    test(reporting): register mass precision smoke

--- tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
@@ -5,6 +5,7 @@ namespace QS3D.Core.SmokeTests
         internal static void RunAll()
         {
             ProjectQuantitySmoke.Run();
+            QuantityReportMassPrecisionSmoke.Run();
             QuantityRuleCanonicalNumericKeySmoke.Run();
             QuantityRuleTokenCanonicalitySmoke.Run();
             QuantityReportDistinctNoteSmoke.Run();
