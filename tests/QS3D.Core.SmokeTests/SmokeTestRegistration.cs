f72c2426f539e9ab89af3279b226f2941ab579f0
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-19 15:25:21 +0700

    test(reporting): restore selection integrity registration

--- tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
@@ -5,6 +5,7 @@ namespace QS3D.Core.SmokeTests
         internal static void RunAll()
         {
             ProjectQuantitySmoke.Run();
+            QuantityReportSelectionCountIntegritySmoke.Run();
             QuantityRuleCanonicalNumericKeySmoke.Run();
             QuantityRuleTokenCanonicalitySmoke.Run();
             QuantityReportDistinctNoteSmoke.Run();
