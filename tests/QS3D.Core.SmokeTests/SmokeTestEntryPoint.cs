c6c2417f90d5e09bebd80c89113e9f0a9cbf54e1
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-24 22:37:34 +0700

    test(coordination): register bulk dirty atomicity smoke (#3730)

--- tests/QS3D.Core.SmokeTests/SmokeTestEntryPoint.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestEntryPoint.cs
@@ -21,6 +21,7 @@ namespace QS3D.Core.SmokeTests
                 CoordinationIssuePersistenceSmoke.Run();
                 CoordinationIssueExcelLifecycleSmoke.Run();
                 CoordinationIssueExcelWorkbookSmoke.Run();
+                CoordinationDirtyBatchAtomicitySmoke.Run();
                 WallJunctionSnapPreviewRevisionSmoke.Run();
                 ProjectElementKeyControlSmoke.Run();
                 ProjectMeasurementWorkItemMappingIdentitySmoke.Run();
