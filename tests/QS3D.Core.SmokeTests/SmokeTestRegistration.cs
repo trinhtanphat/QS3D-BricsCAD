92ec04fbc3b93c82c764ac3e8b4da78de58772d0
longnguyentuan2107-maker <long.nguyentuan2107@gmail.com>
2026-08-23 21:57:13 -0700

    test(core): register structural wall formwork smoke

--- tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
+++ tests/QS3D.Core.SmokeTests/SmokeTestRegistration.cs
@@ -25,6 +25,7 @@ namespace QS3D.Core.SmokeTests
             WallQuantityOpeningBoundSmoke.Run();
             WallOpeningHostCanonicalitySmoke.Run();
             WallQuantityMeasurementTraceSmoke.Run();
+            StructuralWallFormworkSmoke.Run();
             QuantityCalculationBltCompatibilityPresetSmoke.Run();
             QuantityFormworkCalculationEngineSmoke.Run();
             LegacyQuantityReportIdentitySmoke.Run();
