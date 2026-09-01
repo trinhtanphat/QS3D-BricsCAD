a2c7e738f8d5046569744b2eb04e98d6d24ba7a1
longnguyentuan2107-maker <long.nguyentuan2107@gmail.com>
2026-09-01 12:18:21 +0700

    revert: drop unproven DependencyGraph acquisition change

--- src/QS3D.Core/Services/DependencyGraph.cs
+++ src/QS3D.Core/Services/DependencyGraph.cs
@@ -46,10 +46,8 @@ namespace QS3D.Core.Services
             var processedDependencies = new List<KeyValuePair<ProjectElement, HashSet<string>>>();
             var enumerationVersion = _rebuildVersion;
             var elementCount = 0;
-            RequireStableKnownCount(elements, knownCount, knownCountSources, "Dependency graph rebuild");
             using (var enumerator = elements.GetEnumerator())
             {
-                RequireStableKnownCount(elements, knownCount, knownCountSources, "Dependency graph rebuild");
                 while (true)
                 {
                     RequireStableKnownCount(elements, knownCount, knownCountSources, "Dependency graph rebuild");
@@ -168,10 +166,8 @@ namespace QS3D.Core.Services
             var knownCount = RejectKnownOversizedInput(elements, "Dependency ordering", out var knownCountSources);
             var materialized = new List<ProjectElement>();
             var snapshots = new List<OrderingSnapshot>();
-            RequireStableKnownCount(elements, knownCount, knownCountSources, "Dependency ordering");
             using (var enumerator = elements.GetEnumerator())
             {
-                RequireStableKnownCount(elements, knownCount, knownCountSources, "Dependency ordering");
                 while (true)
                 {
                     RequireStableKnownCount(elements, knownCount, knownCountSources, "Dependency ordering");
