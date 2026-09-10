23bd66a4ff102389003642bf72211c0c2c64f851
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-09-10 15:56:23 +0700

    test(core): keep SourceHandle fixture transition-safe #6340

--- tests/QS3D.Core.SmokeTests/SourceHandleRootFreshnessSmoke.cs
+++ tests/QS3D.Core.SmokeTests/SourceHandleRootFreshnessSmoke.cs
@@ -60,7 +60,10 @@ namespace QS3D.Core.SmokeTests
         {
             var element = new ProjectElement("E-LATE", ElementCategory.ArchitecturalWall);
             element.SourceHandles.Add("DEF");
+            var beforeAdd = project.ChangeVersion;
             project.Elements.Add(element);
+            if (project.ChangeVersion == beforeAdd)
+                project.Touch();
             yield return element.Id;
         }
 
