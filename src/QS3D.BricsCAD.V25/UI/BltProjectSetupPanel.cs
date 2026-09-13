ea320fd570fca6f15207ad15e66b0680b8d4b0f3
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-09-12 17:39:08 -0700

    fix(v25): disambiguate BricsCAD application generation guard

--- src/QS3D.BricsCAD.V25/UI/BltProjectSetupPanel.cs
+++ src/QS3D.BricsCAD.V25/UI/BltProjectSetupPanel.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using Bricscad.ApplicationServices;
 using QS3D.Core.Domain;
+using Application = Bricscad.ApplicationServices.Application;
 
 namespace QS3D.BricsCAD.V25.UI
 {
