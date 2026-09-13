7d9d7219192dedc60932b0a188bd679a01cccb8a
trinhtanphat <trinhtanphat0000@gmail.com>
2026-09-14 00:05:27 +0700

    test(local022): admit matched V25/V26 qualification pair #6189

--- scripts/test-bricscad-v25-single-footing.ps1
+++ scripts/test-bricscad-v25-single-footing.ps1
@@ -88,6 +88,10 @@ $candidates = @{
         PackageSha256 = '4d9869e38682674772196a3e238f115624ff357a276bb0b976000b63c9a833b5'
         ProductVersion = '0.1.0-preview.10307'; Kind = 'LOCAL_PR_CANDIDATE'
     }
+    'd5e5e3851125b279bc6807074a34f8de2700cef5' = @{
+        PackageSha256 = 'e27d645b88af709369ac8c04b96fff43b8c688496b8908694825b7491efc2633'
+        ProductVersion = '0.2.0-preview.14'; Kind = 'LOCAL_PR_CANDIDATE'
+    }
 }
 if (-not $candidates.ContainsKey($ProductSourceSha)) { throw 'Unallocated LOCAL-022 product source.' }
 $candidate = $candidates[$ProductSourceSha]
