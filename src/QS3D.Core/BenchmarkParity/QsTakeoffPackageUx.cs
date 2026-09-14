1cc7b36b0cc6640a1d3fcf06daf72b9fa914d3af
trinhtanphat <trinhtanphat0000@gmail.com>
2026-09-14 22:38:16 +0700

    fix(c02): canonicalize package cost aggregation #6970

--- src/QS3D.Core/BenchmarkParity/QsTakeoffPackageUx.cs
+++ src/QS3D.Core/BenchmarkParity/QsTakeoffPackageUx.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using QS3D.Core.Reporting;
 
 namespace QS3D.Core.BenchmarkParity
 {
@@ -100,20 +101,20 @@ namespace QS3D.Core.BenchmarkParity
 
         private static double SumEstimatedCosts(IEnumerable<TakeoffWorkflowLine> inventory)
         {
-            var sum = 0d;
-            var compensation = 0d;
+            var accumulator = new QuantityReportMath.FiniteAccumulator();
             foreach (var line in inventory)
             {
-                var value = line.EstimatedCost;
-                var next = sum + value;
-                compensation += Math.Abs(sum) >= Math.Abs(value)
-                    ? (sum - next) + value
-                    : (value - next) + sum;
-                sum = next;
+                try
+                {
+                    accumulator.Add(line.EstimatedCost, "estimatedCost");
+                }
+                catch (OverflowException)
+                {
+                    throw new ArgumentOutOfRangeException("estimatedCost", "must be finite");
+                }
             }
 
-            var total = QsModelElementSnapshot.Finite(sum + compensation, "estimatedCost");
-            return total == 0d ? 0d : total;
+            return accumulator.Value("estimatedCost");
         }
     }
 
